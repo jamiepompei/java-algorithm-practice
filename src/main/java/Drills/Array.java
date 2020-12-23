78a3b5af5b7fe9374382b43502a3a2522c75d070
nicolepompei <nicole.lynn.pompei@gmail.com>
2020-12-22 19:49:36 -0500

    second largest num problem

--- src/main/java/Drills/Array.java
+++ src/main/java/Drills/Array.java
@@ -1,4 +1,29 @@
 package Drills;
 
 public class Array {
+    //write a method that will return the second largest num in an array of non-sorted unique positive ints.
+
+    public int secondLargestNum(int[] array){
+        //assume that the largest value is the first value in the array
+        int max = array[0];
+        int secondLargestNum = 0;
+
+        //if the array is empty, return -1
+        if(array.lenght == 0){
+            return - 1;
+        }
+
+        for (int i = 1; i < array.length - 1 ; i++) {
+        //if the value at index 1 (one to the right of the starting value, which is max, is greater than the mex value,
+            if(array[i] > max){
+                //assign max to a temporary value
+                int temp = array[i];
+                //assign max to the secondLargestNum, as the previous max is now the second largest
+                secondLargestNum = max;
+                //assign temp to max, as temp is handing off the maximum value
+                max = temp;
+            }
+        }
+        return secondLargestNum;
+    }
 }

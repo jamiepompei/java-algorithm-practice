d5974b9683532d2533229e62f66c8f4b245d6a1f
nicolepompei <nicole.lynn.pompei@gmail.com>
2020-12-23 14:49:10 -0500

    count duplicates solution with stream, count duplicates solution via sort

--- src/main/java/LeetCode/LeetCode.java
+++ src/main/java/LeetCode/LeetCode.java
@@ -1,5 +1,8 @@
 package LeetCode;
 
+import java.util.Arrays;
+import java.util.stream.Collectors;
+
 public class LeetCode {
 
     /**
@@ -37,4 +40,56 @@ public class LeetCode {
             end--;
         }
     }
+
+
+    /**
+     * Contains Duplicate
+     *
+     * Given an array of integers, find if the array contains any duplicates.
+     *
+     * Yur function should return true if any value appears at least twice in the array, and it should return false if every element is distinct.
+     *
+     * Example 1
+     * Input: [1,2,3,1]
+     * Output: true
+     *
+     * Example 2
+     * Input: [1,2,3,4]
+     * Output: false
+     *
+     * Example 3
+     * Input: [1,1,1,3,3,4,3,2,4,2]
+     * Output: true
+     */
+
+    //Solution #1 with a stream
+    public boolean containsDuplicate(int[] nums){
+
+        if(nums.length <= 1){
+            return false;
+        }
+
+        List<integer> numList = Arrays.stream(nums)
+                        .distinct()
+                            .boxed()
+                                .collection(Collectors.toList());
+
+        return numList.size() != nums.length;
+    }
+
+    //Solution #2 Sorting
+    public boolean containsDuplicate(int[] nums){
+        Arrays.sort(nums);
+
+        for (int i = 0; i < nums.length - 1; i++) {
+            if(array[i] == array[i+1]){
+                return true;
+            }
+        }
+        return false;
+    }
+
+
+
+
 }

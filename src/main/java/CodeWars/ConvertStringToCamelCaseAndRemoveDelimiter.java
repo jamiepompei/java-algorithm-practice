84bfb433484aca9dede729c62c07da81640fa6b4
nicolepompei <nicole.lynn.pompei@gmail.com>
2021-01-23 18:54:06 -0500

    convert string to camel case and remove delimiter alternate solution

--- src/main/java/CodeWars/ConvertStringToCamelCaseAndRemoveDelimiter.java
+++ src/main/java/CodeWars/ConvertStringToCamelCaseAndRemoveDelimiter.java
@@ -23,15 +23,36 @@ public class ConvertStringToCamelCaseAndRemoveDelimiter {
             }
         }
         StringBuilder sb = new StringBuilder(s);
-
+    //if the current value == '-' or '_' delete it
         for(int i = 0; i< sb.length(); i++){
             if(s.charAt(i) == '-' || s.charAt(i) == '_'){
                 sb.deleteCharAt(i);
+            //replace that previous symbols value with the srting value of the upper case version of the character
                 sb.replace(i, i+1, String.valueOf(Character.toUpperCase(sb.charAt(i))));
             }
         }
 
         return sb.toString();
+    }
+
+    //alternate solution with char array
+    public String convertCamelCaseCharArray(String s){
+        String result = "";
+        char[] sAsCharArray = s.toCharArray();
+
+        for(int i = 0; i < s.length(); i++){
+            //if the current value in the array equals one of the cymbals
+            if((sAsCharArray[i] == '_') || (sAsCharArray[i] == '-')){
+                //append the uppercase string value of the character to the empty string
+                result += Character.toString(sAsCharArray[i+1]).toUpperCase();
+            } else{
+                //else append the char to the string
+                result += sAsCharArray[i];
+            }
+        }
+        //return the string
+        return result;
+
     }
 
 }

8bd1382adb14454ae39ec43fbdee5d3f2d12f960
nicolepompei <nicole.lynn.pompei@gmail.com>
2021-01-25 08:52:51 -0500

    print all characters that occur more than once in a string

---
+++ src/main/java/OtherSources/FindDuplicatesInString.java
@@ -0,0 +1,33 @@
+package OtherSources;
+
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+
+public class FindDuplicatesInString {
+
+    public void printAllDupsInString(String str){
+
+        char[] charArray = str.toCharArray();
+        Map<Character, Integer> map = new HashMap<Character, Integer>();
+
+        for(Character ch : charArray){
+            if(map.containsKey(ch)){
+                map.put(ch, map.get(ch) + 1);
+            } else{
+                map.put(ch, 1);
+            }
+        }
+
+       //iterate through the map to print each character that occurs more than 1x
+        Set<Map.Entry<Character, Integer>> entrySet = map.entrySet();
+        for(Map.Entry<Character, Integer> entry : entrySet){
+            if(entry.getValue() > 1){
+                System.out.println(entry.getKey() + " : " + entry.getValue());
+            }
+        }
+
+
+    }
+}

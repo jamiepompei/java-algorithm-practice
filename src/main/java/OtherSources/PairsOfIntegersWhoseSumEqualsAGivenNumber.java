7b34a5f8935e99b1dec1de61380f0942705a8d29
nicolepompei <nicole.lynn.pompei@gmail.com>
2021-01-26 20:11:02 -0500

    pairs of integers whose sum equals a given num solution two pointer approach

--- src/main/java/OtherSources/PairsOfIntegersWhoseSumEqualsAGivenNumber.java
+++ src/main/java/OtherSources/PairsOfIntegersWhoseSumEqualsAGivenNumber.java
@@ -1,5 +1,6 @@
 package OtherSources;
 
+import java.util.Arrays;
 import java.util.HashSet;
 import java.util.Set;
 
@@ -7,7 +8,8 @@ import java.util.Set;
  * Find the pairs of integers who sum is equal to a given number
  */
 public class PairsOfIntegersWhoseSumEqualsAGivenNumber {
-
+//internally uses a hash map, so not the most effeciant solution if there is a large array and
+    //memory is a concern
     public int[] sumOfInts(int[] array, int sum){
         //x+y = sum
         //given y = sum - x
@@ -19,6 +21,27 @@ public class PairsOfIntegersWhoseSumEqualsAGivenNumber {
                 return new int[] {potentialMatch, number};
             } else{
                 nums.add(number);
+            }
+        }
+        return new int[0];
+    }
+
+//two pointer approach is more effecient is memory is a concern
+    public int[] sumOfNums(int[] array, int targetSum){
+        int left = 0;
+        int right = array.length - 1;
+
+        Arrays.sort(array);
+
+        while(left < right){
+            int currentSum = array[left] + array[right];
+
+            if(currentSum == targetSum){
+                return new int[] {left, right};
+            } else if(currentSum > targetSum){
+                right -= 1;
+            } else{
+                left += 1;
             }
         }
         return new int[0];

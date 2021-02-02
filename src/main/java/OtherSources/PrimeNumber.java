a180576e0c8146e958676b9491614ab958365d3f
nicolepompei <nicole.lynn.pompei@gmail.com>
2021-02-02 13:10:50 -0500

    prime number practice

--- src/main/java/OtherSources/PrimeNumber.java
+++ src/main/java/OtherSources/PrimeNumber.java
@@ -5,19 +5,38 @@ public class PrimeNumber {
     public void primeNumberChecker(int num){
 
 
-        boolean isPrime = false;
+        boolean isPrime = true;
         //loop from 2 to num/2. we can loop up to halfway point, bc if it's multiple of 2, its not prime
         for(int i = 2; i <= num / 2; i ++){
-            //if a num is prime, break out of the loop, this determines num is not a prime number
+            //if a num is divisible by any num that is not 1 or itself, break out of the loop, this determines num is not a prime number
             if(num % i == 0){
-                isPrime = true;
+                isPrime = false;
                 break;
             }
         }
-        if(!isPrime){
+        if(isPrime){
             System.out.println(num + " is a prime number");
         } else{
             System.out.println(num + " is not a prime number");
         }
+    }
+
+    public void primeNumPractce(Integer num){
+        boolean isPrime = true;
+
+        for(int i =2; i < num/2; i++){
+            //if num is divisible by any other num that is not 1 or itself, it is not prime
+            if(num % i == 0){
+                isPrime = false;
+                break;
+            }
+        }
+        if(isPrime){
+            System.out.println(num + " is a prime number") ;
+        }else{
+            System.out.println(num + " is not a prime number");
+        }
+
+
     }
 }
